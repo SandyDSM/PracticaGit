3116bde12202e06e1f6b4dfd4b08b4b195f93368
Sandra de los Santos <sad.zoe@gmail.com>
2021-10-12 00:38:22 -0500

    Comentarios A

--- Archivo2_B.js
+++ Archivo2_B.js
@@ -1,1 +1,2 @@
 console.log("Este es el archivo 2 hecho por B");
+// Comentario hecho por el participante A
--- Archivo3_B.js
+++ Archivo3_B.js
@@ -1,1 +1,2 @@
 console.log("Este es el archivo 3 hecho por B");
+// Comentario hecho por el participante A
